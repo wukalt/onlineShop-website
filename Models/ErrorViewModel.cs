627d03e607752eea5c0f798c0c058df4345a384c
wuKa <wuka.sec@gmail.com>
2025-05-19 19:52:34 +0330

    Initial dotnet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace onlineShop_website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
